9cecca5dc62313972b5d476ca83de454c54b56e9
Sumanth Dodda <51809378+sumo2001@users.noreply.github.com>
2021-11-01 23:30:12 +0530

    Update nuclear.sh

--- Might/nuclear.sh
+++ Might/nuclear.sh
@@ -2,5 +2,5 @@
 
 dir=./Results/$1
 
-cat $dir/$1_probed | ./go/bin/nuclei -c 500 -silent -t ./nova/nuclei-templates/ -o $dir/$1_nuclei;
+cat $dir/$1_probed | ~/go/bin/nuclei -c 500 -silent -t ./nova/nuclei-templates/ -o $dir/$1_nuclei;
 
