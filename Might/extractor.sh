1201bc1fcb38e1f3a1be68d6486d50267707b97c
Sumanth Dodda <51809378+sumo2001@users.noreply.github.com>
2021-11-01 23:27:12 +0530

    Update extractor.sh

--- Might/extractor.sh
+++ Might/extractor.sh
@@ -2,5 +2,5 @@
 
 dir=./Results/$1
 
-./go/bin/waybackurls $1 > $dir/$1_extraction;
-./go/bin/gau $1 >> $dir/$1_extraction; cat $dir/$1_extraction | sort -u > $dir/$1_archive_extracts;
+~/go/bin/waybackurls $1 > $dir/$1_extraction;
+~/go/bin/gau $1 >> $dir/$1_extraction; cat $dir/$1_extraction | sort -u > $dir/$1_archive_extracts;
