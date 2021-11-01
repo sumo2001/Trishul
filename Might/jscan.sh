01c8b8d9da11226d68ed35b710c4d158835a0a2f
Sumanth Dodda <51809378+sumo2001@users.noreply.github.com>
2021-11-01 23:29:16 +0530

    Update jscan.sh

--- Might/jscan.sh
+++ Might/jscan.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 dir=./Results/$1
 
-echo $1| ./go/bin/gau | grep -iE "\.js$" | uniq | sort >> $dir/$1_jsfile_links;
-echo $1 | ./go/bin/subjs >> $dir/$1_jsfile_links;
-echo $1 | ./go/bin/hakrawler -js -depth 2 -scope subs -plain >> $dir/$1_jsfile_links;
+echo $1| ~/go/bin/gau | grep -iE "\.js$" | uniq | sort >> $dir/$1_jsfile_links;
+echo $1 | ~/go/bin/subjs >> $dir/$1_jsfile_links;
+echo $1 | ~/go/bin/hakrawler -s -depth 2 -scope subs -plain >> $dir/$1_jsfile_links;
 
-cat $dir/$1_jsfile_links | ./go/bin/httpx -follow-redirects -silent -status-code | grep "[200]" | cut -d ' ' -f1 | sort -u > $dir/$1_live_jsfile_links;
+cat $dir/$1_jsfile_links | ~/go/bin/httpx -follow-redirects -silent -status-code | grep "[200]" | cut -d ' ' -f1 | sort -u > $dir/$1_live_jsfile_links;
 
 python3 ./nova/LinkFinder/linkfinder.py -d -i $1 -o cli >> $dir/$1_JSEndpoints;
 
